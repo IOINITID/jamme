dbbbc506c1e246689faf590abc46a096d7b6d25f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-10-21 14:34:31 +0300

    Update project files

--- js/script.js
+++ js/script.js
@@ -1,42 +1,19 @@
 $('.slider-one').slick({
   slidesToShow: 8,
   slidesToScroll: 8,
-  // autoplay: true,
-  // autoplaySpeed: 5000,
+  autoplay: true,
+  autoplaySpeed: 5000,
   arrows: false,
   dots: true,
-  // responsive: [{
-  //   breakpoint: 768,
-  //   settings: {
-  //     arrows: true,
-  //     slidesToShow: 1,
-  //     dots: false
-  //   }
-  // }]
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      arrows: true,
+      slidesToShow: 1,
+      dots: false
+    }
+  }]
 });
-// $('.slider-two').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-//   arrows: false,
-//   dots: true,
-//   responsive: [{
-//     breakpoint: 768,
-//     settings: {
-//       arrows: true,
-//       slidesToShow: 1,
-//       dots: false
-//     }
-//   }]
-// });
-
-// $(document).ready(function(){
-// });
-
-// $('.tabone').on('click', function () {
-//   $('.slider-one').slick();
-// });
 
 $('.data-product .info .info__link').on('click', function () {
   $('#modal-form').modal('show');
