738acfa452e4d981c178b12a165a1ba73552c015
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-10-21 12:44:51 +0300

    Update project files

--- js/script.js
+++ js/script.js
@@ -1,16 +1,55 @@
 $('.slider-one').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
+  slidesToShow: 8,
+  slidesToScroll: 8,
+  // autoplay: true,
+  // autoplaySpeed: 5000,
   arrows: false,
   dots: true,
-  responsive: [{
-    breakpoint: 768,
-    settings: {
-      arrows: true,
-      slidesToShow: 1,
-      dots: false
-    }
-  }]
+  // responsive: [{
+  //   breakpoint: 768,
+  //   settings: {
+  //     arrows: true,
+  //     slidesToShow: 1,
+  //     dots: false
+  //   }
+  // }]
+});
+// $('.slider-two').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 5000,
+//   arrows: false,
+//   dots: true,
+//   responsive: [{
+//     breakpoint: 768,
+//     settings: {
+//       arrows: true,
+//       slidesToShow: 1,
+//       dots: false
+//     }
+//   }]
+// });
+
+// $(document).ready(function(){
+// });
+
+// $('.tabone').on('click', function () {
+//   $('.slider-one').slick();
+// });
+
+$('.data-product .info .info__link').on('click', function () {
+  $('#modal-form').modal('show');
+});
+
+$(function () {
+  $("#tabs").tabs();
+});
+
+$('.tabtwo').on('click', function () {
+  $('.assortment__line').addClass('line-right');
+});
+
+$('.tabone').on('click', function () {
+  $('.assortment__line').removeClass('line-right');
 });
